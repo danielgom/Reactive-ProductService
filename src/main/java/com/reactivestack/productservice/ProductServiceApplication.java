fffd429bfbe203ee80d69303de7e59a43ad28bec
Daniel Gomez A <33067010+danielgom@users.noreply.github.com>
2022-02-28 17:20:01 -0600

    feature/16 (#18)

--- src/main/java/com/reactivestack/productservice/ProductServiceApplication.java
+++ src/main/java/com/reactivestack/productservice/ProductServiceApplication.java
@@ -8,13 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(
 		title = "Product service",
-		version = "1.0",
+		version = "2.0",
 		description = "Reactive product service"
 ))
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
