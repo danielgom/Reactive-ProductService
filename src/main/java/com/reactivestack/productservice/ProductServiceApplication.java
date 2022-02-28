65453723a614ac6596a38c041218d93e5f1f1d9b
Daniel <dga_355@hotmail.com>
2022-02-28 16:54:55 -0600

    feature/15

--- src/main/java/com/reactivestack/productservice/ProductServiceApplication.java
+++ src/main/java/com/reactivestack/productservice/ProductServiceApplication.java
@@ -14,6 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
+
 }
